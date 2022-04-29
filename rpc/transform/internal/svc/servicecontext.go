78a69ca22b267656698c95edff240ce4c71283f7
zhangyulong03 <zhangyulong03@baidu.com>
2022-04-29 14:55:46 +0800

    init grpc model with cache

--- rpc/transform/internal/svc/servicecontext.go
+++ rpc/transform/internal/svc/servicecontext.go
@@ -1,9 +1,13 @@
 package svc
 
-import "shorturl.com/shorturl/rpc/transform/internal/config"
+import (
+	"shorturl.com/shorturl/rpc/transform/internal/config"
+	"shorturl.com/shorturl/rpc/transform/model"
+)
 
 type ServiceContext struct {
 	Config config.Config
+	Model  model.ShorturlModel
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
